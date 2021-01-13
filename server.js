1041dc9daf6f19f36062dc1921dde39139c6f512
Marco Antonio De la cruz Santos <marco033F@hotmail.com>
2021-01-13 01:06:10 -0400

    Allow CORS in Express

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const port = process.env.PORT || 8080
 app.listen(port)
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization");
+    res.header("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
 	next();
 });
