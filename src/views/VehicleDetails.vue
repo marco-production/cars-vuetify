acf8df66b296244bd0bd0a07e5ac4ad8bf68d576
Marco Antonio De la cruz Santos <marco033F@hotmail.com>
2021-03-07 22:48:35 -0400

    Get vehicle details data from API

--- src/views/VehicleDetails.vue
+++ src/views/VehicleDetails.vue
@@ -1,15 +1,49 @@
 <template>
     <div>
-        <h1>{{ $route.params.slug }}</h1>
-        <h2>{{slug}}</h2>
+        <v-container>
+            <v-row justify="space-around">
+                <v-col md="6" cols="12">
+                    <v-img
+                        :src="vehicle.image"
+                        aspect-ratio="2"
+                        contain
+                    ></v-img>
+                </v-col>
+                <v-col md="6" cols="12">
+                    <h2 class="mb-3 text-decoration-underline">{{ vehicle.make.name }} {{ vehicle.model.name }} {{ vehicle.year }}</h2>
+                    <p><span class="font-weight-bold">Vehicle Type:</span> {{ vehicle.vehicle_type.name }}</p>
+                    <p><span class="font-weight-bold">Fuel:</span> {{ vehicle.fuel.name }}</p>
+                    <p><span class="font-weight-bold">Chassis:</span> {{ vehicle.chassis }}</p>
+                    <p><span class="font-weight-bold">Condition:</span> {{ vehicle.condition == 1 ? 'New' : 'Used' }}</p>
+                    <p><span class="font-weight-bold">Description:</span> {{ vehicle.description }}</p>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
-            slug: this.$route.params.slug
+            slug: this.$route.params.slug,
+            apiToken: 'yJsEhmB5HpnuvPMu',
+            vehicle: []
         }
     },
+
+    created(){
+        this.getVehicle()
+    },
+
+    methods:{
+        getVehicle(){
+            axios.get(`${this.$apiUrl}/vehicles/${this.slug}`, {headers: {'api_token': this.apiToken }})
+            .then((vehicle)=>{
+                this.vehicle = vehicle.data
+            })
+        }
+    }
 }
 </script>
